5df91b453f62502bd909ea04219ca98047ada8e2
nino-mau <nino.mauuary@gmail.com>
2025-11-18 12:24:45 +0100

    :package:build(dependencies): install and set up better-auth

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,12 +1,5 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const Users = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique()
-});
-
 export const Test = pgTable('test', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   label: varchar({ length: 255 }).notNull()
