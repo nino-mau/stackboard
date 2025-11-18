ab4398bda05561899915e704f2f4f8451f63b8e9
nino-mau <nino.mauuary@gmail.com>
2025-11-18 12:24:45 +0100

    :tada:init: set up better-auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/schema/schema.ts',
+  schema: ['./src/schema/schema.ts', './src/schema/auth-schema.ts'],
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!
