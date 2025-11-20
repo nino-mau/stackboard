276b1ae348d1c2f03bdd900fcef859c3e90582c7
nino-mau <nino.mauuary@gmail.com>
2025-11-20 12:23:23 +0100

    🛠 refactor(css): move  assets dir to src/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './assets/css/main.css';
+import '@/assets/css/main.css';
 import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
