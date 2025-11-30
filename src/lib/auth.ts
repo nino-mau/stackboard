7f4b2689900fb16708682eae39682e99af12e564
nino.mauuary <nino.mauuary@gmail.com>
2025-11-30 17:40:59 +0100

    :bug:fix(better-auth): pass the authSchema to better auth drizzleAdapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,12 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/lib/db'; // your drizzle instance
 import { nextCookies } from 'better-auth/next-js';
+import * as authSchema from '@/schema/auth-schema';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema: authSchema
   }),
   plugins: [nextCookies()],
 });
