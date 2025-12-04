ef8e5f8f17a1267b82663ad2ba593984e70fff1f
nino.mauuary <nino.mauuary@gmail.com>
2025-12-04 21:08:02 +0100

    ✨ feat(shadcn): add shadcn toast with custom styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
