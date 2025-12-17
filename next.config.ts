62ecb664b99f46e4a5d7290296abdfebca4d2c0a
nino.mauuary <nino.mauuary@gmail.com>
2025-12-17 22:28:08 +0100

    ✨ feat(routing): redirect root to projects page & remove unused root page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
