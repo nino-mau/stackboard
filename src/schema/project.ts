669bb618fd63702d0d76d6fa0cd39c68f726098e
nino.mauuary <nino.mauuary@gmail.com>
2025-12-23 00:26:40 +0100

    ✨ feat(schema): add project and projectPage tables

---
+++ src/schema/project.ts
@@ -0,0 +1,49 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  varchar,
+  timestamp,
+  pgEnum
+} from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import { user } from './auth';
+
+/**
+ * Timestamp columns
+ */
+const timestamps = {
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date())
+};
+
+export const projectLogoType = pgEnum('projectLogoType', ['icon', 'emoji']);
+
+export const project = pgTable('project', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  name: text().notNull(),
+  description: text(),
+  repoUrl: text(),
+  logoType: text(),
+  logoIconName: text(),
+  logoEmoji: text(),
+  creatorId: uuid('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }),
+  ...timestamps
+});
+
+export const projectPage = pgTable('project_page', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  projectId: uuid('project_id')
+    .notNull()
+    .references(() => project.id, { onDelete: 'cascade' }),
+  ...timestamps
+});
