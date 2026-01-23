5004b8589a77db271509548c3bbab757ed4dd6cd
nino.mauuary <nino.mauuary@gmail.com>
2026-01-23 19:48:06 +0100

    🔧 chore: remove 'use client' statements

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,7 +1,5 @@
-'use client';
-
-import type * as React from 'react';
 import { Popover as PopoverPrimitive } from '@base-ui/react/popover';
+import type * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
